d032a223e4d1cc2372fd2faf51448909f5b55d56
Lam <lamitptit99@gmail.com>
2019-07-16 23:40:17 +0700

    hayzz

---
+++ app/src/main/java/com/example/saira_000/stickynotes/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.saira_000.stickynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+class Option{
+    public String topic,content;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView autoCompleteTextView;
+    Button btnadd, btnsearch;
+    EditText editText;
+    String[] Topic = {""};
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ArrayList<Option> arrayList = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, Topic);
+        editText = (EditText) findViewById(R.id.content);
+        btnadd = (Button) findViewById(R.id.add);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String hint = autoCompleteTextView.getText().toString().trim();
+                String contenT = editText.getText().toString().trim();
+                System.out.println(hint + "\n" + contenT);
+                if (!hint.equals("") && !contenT.equals("")) {
+                    Option op = new Option();
+                    op.setTopic(hint);
+                    op.setContent(contenT);
+                    if (isExist(hint) == -1) {
+                        adapter.add(op.getTopic().toString());
+                    }
+                    arrayList.add(op);
+                    autoCompleteTextView.setText("");
+                    editText.setText("");
+                }
+            }
+        });
+        btnsearch = (Button) findViewById(R.id.search);
+        btnsearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean canFind = false;
+                String findtopic = autoCompleteTextView.getText().toString().trim();
+                for (Option s : arrayList) {
+                    if (s.getTopic().equals(findtopic)) {
+                        editText.setText(s.getContent());
+                        canFind = true;
+                        break;
+                    }
+                }
+                if (canFind == false) {
+                    Toast.makeText(MainActivity.this, "Can not find. Plz Add", Toast.LENGTH_SHORT).show();
+                    editText.requestFocus();
+                }
+            }
+        });
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.auto);
+        autoCompleteTextView.setThreshold(1);//start matching from first char
+        autoCompleteTextView.setAdapter(adapter);//set adapter to save what you searched
+    }
+
+    public int isExist(String s) {
+        for (String tmp : Topic) {
+            if (tmp.equals(s)) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+}
